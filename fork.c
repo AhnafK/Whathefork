282540cb29f5537db7e2d1389c5d4723257bb6ae
AhnafK <akazi1@stuy.edu>
2018-11-18 20:52:46 -0500

    done

--- fork.c
+++ fork.c
@@ -2,11 +2,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
-int chil
+#include <time.h>
 
 int main(){
-  if(!fork()){
-    printf("%d",getpid());
+  srand(time(NULL));
+  int q = rand()%15*5;
+  int qw = rand()%15*5;
+  if(fork()){
+    if(fork()){
+      int status; 
+      int cpid = wait(&status); 
+      printf("%d ran for %d seconds.\n",cpid,WEXITSTATUS(status));
+      return 0;
+    }
+    else{
+      printf("%d\n",getpid());
+      sleep(q);
+      printf("%d is finished\n", getpid());
+      return q;
+    }
   }
+  else{
+      printf("%d\n",getpid());
+      sleep(qw);
+      printf("%d is finished\n", getpid());
+      return qw;
+  }
+  int status; 
+  int cpid = wait(&status); 
+  printf("%d ran for %d seconds.\n",cpid,WEXITSTATUS(status));
+  printf("parent process done");
+  return 0;
 }
